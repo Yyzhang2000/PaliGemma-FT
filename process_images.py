188b2585d3ee96f4055f4a4f1c8bd3cc4fea4c13
YuyangZhang <yuyangzhang2025@gmail.com>
2025-04-10 23:27:51 +0800

    Fixed bug

--- process_images.py
+++ process_images.py
@@ -8,18 +8,11 @@ IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
 
 
 def add_image_tokens_to_prompt(prefix_prompt, bos_token, image_seq_len, image_token):
-    # Quoting from the blog (https://huggingface.co/blog/paligemma#detailed-inference-process):
-    #   The input text is tokenized normally.
-    #   A <bos> token is added at the beginning, and an additional newline token (\n) is appended.
-    #   This newline token is an essential part of the input prompt the model was trained with, so adding it explicitly ensures it's always there.
-    #   The tokenized text is also prefixed with a fixed number of <image> tokens.
-    # NOTE: from the paper it looks like the `\n` should be tokenized separately, but in the HF implementation this is not done.
-    #       ref to HF implementation: https://github.com/huggingface/transformers/blob/7f79a97399bb52aad8460e1da2f36577d5dccfed/src/transformers/models/paligemma/processing_paligemma.py#L55-L73
     return f"{image_token * image_seq_len}{bos_token}{prefix_prompt}\n"
 
 
 def rescale(
-    image: np.ndarray, scale: float, dtype: np.dtype = np.float32
+    image: np.ndarray, scale: float, dtype: np.dtype = np.dtype(np.float32)
 ) -> np.ndarray:
     rescaled_image = image * scale
     rescaled_image = rescaled_image.astype(dtype)
@@ -27,11 +20,11 @@ def rescale(
 
 
 def resize(
-    image: Image,
+    image: Image.Image,
     size: Tuple[int, int],
-    resample: Image.Resampling = None,
+    resample: Image.Resampling = Image.Resampling.BICUBIC,
     reducing_gap: Optional[int] = None,
-) -> np.ndarray:
+) -> Image.Image:
     height, width = size
     resized_image = image.resize(
         (width, height), resample=resample, reducing_gap=reducing_gap
@@ -52,35 +45,39 @@ def normalize(
 
 def process_images(
     images: List[Image.Image],
-    size: Dict[str, int] = None,
-    resample: Image.Resampling = None,
-    rescale_factor: float = None,
+    size: Tuple[int, int] = (224, 224),
+    resample: Image.Resampling = Image.Resampling.BICUBIC,
+    rescale_factor: Optional[float] = 1 / 255.0,
     image_mean: Optional[Union[float, List[float]]] = None,
     image_std: Optional[Union[float, List[float]]] = None,
 ) -> List[np.ndarray]:
     height, width = size[0], size[1]
-    images = [
+    resized_images = [
         resize(image=image, size=(height, width), resample=resample) for image in images
     ]
     # Convert each image to a numpy array
-    images = [np.array(image) for image in images]
+    image_arrays = [np.array(img) for img in resized_images]
     # Rescale the pixel values to be in the range [0, 1]
-    images = [rescale(image, scale=rescale_factor) for image in images]
+    if rescale_factor is not None:
+        image_arrays = [rescale(image, scale=rescale_factor) for image in image_arrays]
     # Normalize the images to have mean 0 and standard deviation 1
-    images = [normalize(image, mean=image_mean, std=image_std) for image in images]
+    if image_mean is not None and image_std is not None:
+        image_arrays = [
+            normalize(image, mean=image_mean, std=image_std) for image in image_arrays
+        ]
     # Move the channel dimension to the first dimension. The model expects images in the format [Channel, Height, Width]
-    images = [image.transpose(2, 0, 1) for image in images]
-    return images
+    image_arrays = [image.transpose(2, 0, 1) for image in image_arrays]
+    return image_arrays
 
 
 class PaliGemmaProcessor:
     IMAGE_TOKEN = "<image>"
 
-    def __init__(self, tokenizer, num_image_tokens: int, image_size: int):
+    def __init__(self, tokenizer, num_image_tokens: int = 256, image_size: int = 224):
         super().__init__()
 
-        self.image_seq_length = num_image_tokens
-        self.image_size = image_size
+        self.image_seq_length = num_image_tokens  # 256 = (224 // 14)**2
+        self.image_size = image_size  # 224
 
         tokens_to_add = {"additional_special_tokens": [self.IMAGE_TOKEN]}
         EXTRA_TOKENS = [
@@ -123,7 +120,7 @@ class PaliGemmaProcessor:
         # Convert the list of numpy arrays to a single numpy array with shape [Batch_Size, Channel, Height, Width]
         pixel_values = np.stack(pixel_values, axis=0)
         # Convert the numpy array to a PyTorch tensor
-        pixel_values = torch.tensor(pixel_values)
+        pixel_values = torch.tensor(pixel_values).float()
 
         input_strings = [
             add_image_tokens_to_prompt(
