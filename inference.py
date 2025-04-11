9f041ee3c14bb4fd32796e0e755d884bade0f9da
YuyangZhang <yuyangzhang2025@gmail.com>
2025-04-11 16:54:51 +0800

    Add Inference

--- inference.py
+++ inference.py
@@ -2,6 +2,9 @@ from PIL import Image
 import torch
 import fire
 
+
+from typing import Optional
+
 from process_images import PaliGemmaProcessor
 from model.kv_cache import KVCache
 from model.paligemma import PaliGemmaForConditionalGeneration
@@ -47,8 +50,8 @@ def inference(
     model: PaliGemmaForConditionalGeneration,
     processor: PaliGemmaProcessor,
     device: str,
-    prompt: str,
-    image_file_path: str,
+    prompt: Optional[str],
+    image_file_path: Optional[str],
     max_tokens_to_generate: int,
     temperature: float,
     top_p: float,
@@ -63,6 +66,11 @@ def inference(
 
     stop_token = processor.tokenizer.eos_token_id
     generated_tokens = []
+
+    if do_sample:
+        print("Top-P Sampling")
+    else:
+        print("Greedy")
 
     for _ in range(max_tokens_to_generate):
         outputs = model(
@@ -79,18 +87,21 @@ def inference(
             next_token_logits = torch.softmax(next_token_logits / temperature, dim=-1)
             next_token_id = sample_top_p(next_token_logits, top_p=top_p)
         else:
-            next_token_id = torch.argmax(next_token_logits, dim=-1)
+            next_token_id = torch.argmax(next_token_logits, dim=-1, keepdim=True)
 
         next_token_id = next_token_id.squeeze(0)
         generated_tokens.append(next_token_id)
         if next_token_id == stop_token:
             break
         # Append the next token to the input_ids and attention_mask
-        # input_ids = next_token_id.unsqueeze(0)
         input_ids = next_token_id.unsqueeze(0)
         attention_mask = torch.cat(
             [attention_mask, torch.ones((1, 1), device=input_ids.device)], dim=-1
         )
+
+        tempt_text = torch.cat(generated_tokens, dim=-1)
+        decoded = processor.tokenizer.decode(tempt_text, skip_special_tokens=True)
+        print(prompt + decoded)
 
     generated_tokens = torch.cat(generated_tokens, dim=-1)
     decoded = processor.tokenizer.decode(generated_tokens, skip_special_tokens=True)
@@ -99,9 +110,9 @@ def inference(
 
 
 def main(
-    model_path: str = None,
-    prompt: str = None,
-    image_file_path: str = None,
+    model_path: str = "./weights",
+    prompt: Optional[str] = None,
+    image_file_path: Optional[str] = None,
     max_tokens_to_generate: int = 100,
     temperature: float = 0.8,
     top_p: float = 0.9,
@@ -121,6 +132,7 @@ def main(
     print(f"Loading model")
     model, tokenizer = load_hf_model(model_path=model_path, device=device)
     model = model.to(device).eval()
+    print("Model Loaded successful")
 
     num_image_tokens = model.config.vision_config.num_image_tokens
     image_size = model.config.vision_config.image_size
